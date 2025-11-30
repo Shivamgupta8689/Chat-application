79315ab0c79472bff2356520c4e9576080361329
Shivamgupta8689 <shivamgupta86923@gmail.com>
2025-11-30 14:31:49 +0530

    updated code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {Authprovider} from './context/Authprovider.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { SocketProvider } from './context/SocketContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/Authprovider";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Authprovider>
-    <SocketProvider>
-    <App />
-    </SocketProvider>
-    </Authprovider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
-)
+);
